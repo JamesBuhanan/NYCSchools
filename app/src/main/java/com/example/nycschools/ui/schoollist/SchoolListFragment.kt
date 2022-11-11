711efd5af8f50154e81dd563e299fe3fc15732f0
JamesBuhanan <JamesBuhanan@gmail.com>
2022-11-11 10:01:58 -0700

    jetpack compose

--- app/src/main/java/com/example/nycschools/ui/schoollist/SchoolListFragment.kt
+++ app/src/main/java/com/example/nycschools/ui/schoollist/SchoolListFragment.kt
@@ -1,32 +1,42 @@
 package com.example.nycschools.ui.schoollist
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.nycschools.R
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SchoolListFragment : Fragment() {
+
+    private val viewModel: SchoolListViewModel by viewModels()
 
     companion object {
         fun newInstance() = SchoolListFragment()
     }
-
-    private lateinit var viewModel: SchoolListViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_school_list, container, false)
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Text(text = "Hello world.")
+            }
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SchoolListViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        viewModel.schoolList.observe(viewLifecycleOwner) {
+            val test = it
+            val test2 = test
+        }
+
     }
 
 }
--- app/src/main/java/com/example/nycschools/ui/schoollist/SchoolListViewModel.kt
+++ app/src/main/java/com/example/nycschools/ui/schoollist/SchoolListViewModel.kt
@@ -1,21 +1,28 @@
 package com.example.nycschools.ui.schoollist
 
+import android.annotation.SuppressLint
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.nycschools.model.School
 import com.example.nycschools.usecase.GetSchoolList
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@SuppressLint("NullSafeMutableLiveData")
 @HiltViewModel
 class SchoolListViewModel @Inject constructor(
     private val getSchoolList: GetSchoolList
 ) : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _schoolList = MutableLiveData<List<School>>()
+    val schoolList: LiveData<List<School>>
+        get() = _schoolList
+
     init {
         viewModelScope.launch {
-            val schoolList = getSchoolList()
+            _schoolList.value = getSchoolList()
         }
-
     }
 }
